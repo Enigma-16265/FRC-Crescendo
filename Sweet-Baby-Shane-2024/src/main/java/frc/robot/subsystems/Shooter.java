e59d49d768e1f99a4a10e5c762e1e17284379153
Zachariah Juarez <the.nerd.1.programmer@gmail.com>
2024-03-06 18:32:23 -0700

    Fixed Shooter Subsystem

--- Sweet-Baby-Shane-2024/src/main/java/frc/robot/subsystems/Shooter.java
+++ Sweet-Baby-Shane-2024/src/main/java/frc/robot/subsystems/Shooter.java
@@ -7,8 +7,10 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkPIDController;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.logging.DataNetworkTableLog;
+import frc.robot.Constants.ModuleConstants;
 
 // 4 Motors
 // private final CANSparkMax m_shooterFlywheelOneSparkMax;
@@ -39,6 +41,14 @@ public class Shooter extends SubsystemBase
     public static final int kShooterPivotCanID = 18;
     public static final int kShooterFeedCanID = 19;
 
+    private static final double ANGLE_TOLERANCE = 2.0; // degrees
+    private static final double maxAngle = 90.0; // maximum angle
+    private static final double minAngle = 0.0; // minimum angle
+    
+    private static final double kP = 0.1;
+    private static final double kI = 0.0;
+    private static final double kD = 0.0;
+
     // Two fly-wheel motors
     private final CANSparkMax m_shooterFlywheelRightSparkMax;
     private final CANSparkMax m_shooterFlywheelLeftSparkMax;
@@ -46,9 +56,9 @@ public class Shooter extends SubsystemBase
     private final RelativeEncoder m_shooterFlywheelEncoder;
 
     // One Pivot motor
-    private final CANSparkMax m_shooterPivotSparkMax;
-
-    private final RelativeEncoder m_shooterPivotEncoder;
+    private final CANSparkMax        m_shooterPivotSparkMax;
+    private final RelativeEncoder    m_shooterPivotEncoder;
+    private final SparkPIDController m_shooterPIDController;
 
     public Shooter()
     {
@@ -63,8 +73,19 @@ public class Shooter extends SubsystemBase
 
         // Pivot
         m_shooterPivotSparkMax = new CANSparkMax(kShooterPivotCanID, MotorType.kBrushless);
-        m_shooterPivotEncoder = m_shooterPivotSparkMax.getEncoder();
 
+        m_shooterPivotEncoder = m_shooterPivotSparkMax.getEncoder();
+        m_shooterPivotEncoder.setPositionConversionFactor( 1.0 / 36.0 );
+        
+        m_shooterPIDController = m_shooterPivotSparkMax.getPIDController();
+
+        m_shooterPIDController.setFeedbackDevice( m_shooterPivotEncoder );
+
+        m_shooterPIDController.setP(kP);
+        m_shooterPIDController.setI(kI);
+        m_shooterPIDController.setD(kD);
+
+        m_shooterPIDController.setOutputRange(-1.0, 1.0);
     }
 
     public void spin( double speed )
@@ -73,6 +94,15 @@ public class Shooter extends SubsystemBase
         dataLog.publish( "speed", speed );
 
         m_shooterFlywheelRightSparkMax.set(speed);
+
+    }
+
+    public void setTargetAngle(double angle) {
+        // Ensure angle is within limits
+        double targetAngle = Math.max(Math.min(angle, maxAngle), minAngle);
+
+        // Set PID setpoint
+        m_shooterPIDController.setReference(targetAngle * (Math.PI / 180.0), CANSparkMax.ControlType.kPosition);
 
     }
 
